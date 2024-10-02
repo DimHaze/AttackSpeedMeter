2f168235e7dfab18fe95ab1360d0906b2b160ae9
riiiiiiin <myc20046@gmail.com>
2024-10-03 01:02:37 +0800

    Mod Config Support

---
+++ ModConfigs/ASMConfigs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader.Config;
+
+namespace AttackSpeedMeter.ModConfigs
+{
+    public class ASMConfigs: ModConfig
+    {
+        public override ConfigScope Mode => ConfigScope.ClientSide;
+        [DefaultValue(0.6f)]
+        public float HPosition;
+        [DefaultValue(0.05f)]
+        public float VPosition;
+        [Range(100, 500)]
+        [DefaultValue(300)]
+        public int Width;
+    }
+}
